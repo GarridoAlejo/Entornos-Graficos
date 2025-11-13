0ff115ba9fdf1e7d94562280a196227dc311fd95
Alejo Garrido <alejo.garrido0@gmail.com>
2025-11-13 00:54:55 -0300

    PHP-5

---
+++ Practica5_PHP/ejercicio4.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['visitas'])) {
+    $_SESSION['visitas'] = 0;
+}
+
+$_SESSION['visitas']++;
+echo "<p>Visitaste esta página {$_SESSION['visitas']} veces en esta sesión.</p>";
